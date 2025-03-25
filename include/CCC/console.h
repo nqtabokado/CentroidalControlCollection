883568d32faf451dbb1f34bcebafd055376efe00
nqtabokado <nguyen.minhtuan@hachi-x.com>
2025-03-25 10:17:31 +0700

    [CI] debug standalone

--- include/CCC/console.h
+++ include/CCC/console.h
@@ -8,6 +8,6 @@
 #else
 #  include <rclcpp/rclcpp.hpp>
 #  define CCC_ERROR_STREAM(x) RCLCPP_ERROR(rclcpp::get_logger("CCC"), "%s", (x).c_str())
-#  define CCC_WARN_STREAM(x) RCLCPP_WARN(rclcpp::get_logger("CCC"), "%s", (x).c_str())
+#  define CCC_WARN_STREAM(x) RCLCPP_WARN(rclcpp::get_logger("CCC"), "%s", std::string(x).c_str())
 #  define CCC_INFO_STREAM(x) RCLCPP_INFO(rclcpp::get_logger("CCC"), "%s", (x).c_str())
 #endif
